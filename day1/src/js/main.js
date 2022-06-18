48a87baa1c3e59098429e67b860bdfd59f90601f
Nic <42087659+nicm42@users.noreply.github.com>
2022-06-18 14:15:37 +0100

    Day 1: set up for SASS

--- day1/src/js/main.js
+++ day1/src/js/main.js
@@ -1,6 +1,9 @@
-import './style.css'
+import '../scss/styles.scss';
 
-document.querySelector('#app').innerHTML = `
+// import './style.css';
+
+/* document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+ */
