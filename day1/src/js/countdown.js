003ed90ac19875f49d845483096e841d31e42cde
Nic <42087659+nicm42@users.noreply.github.com>
2022-07-02 15:18:21 +0100

    Day 1: moved x-data to separate file

---
+++ day1/src/js/countdown.js
@@ -0,0 +1,10 @@
+export default () => ({
+  timerStarted: false,
+  editing: false,
+  minutes: '15',
+  seconds: '00',
+
+  /* toggle() {
+    this.open = !this.open;
+  }, */
+});
--- day1/src/js/main.js
+++ day1/src/js/main.js
@@ -1,16 +1,10 @@
 import '../scss/styles.scss';
 
 import Alpine from 'alpinejs';
+import countdown from './countdown.js';
 
 window.Alpine = Alpine;
 
+Alpine.data('countdown', countdown);
+
 Alpine.start();
-
-
-// import './style.css';
-
-/* document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
- */
