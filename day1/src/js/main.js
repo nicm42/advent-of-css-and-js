151f8e4afdbd86a0d672830b6104fb6477f5a8c0
Nic <42087659+nicm42@users.noreply.github.com>
2022-07-03 15:06:14 +0100

    Day 1: got countdown working

--- day1/src/js/main.js
+++ day1/src/js/main.js
@@ -1,10 +1,6 @@
 import '../scss/styles.scss';
 
 import Alpine from 'alpinejs';
-import countdown from './countdown.js';
-
 window.Alpine = Alpine;
-
-Alpine.data('countdown', countdown);
 
 Alpine.start();
